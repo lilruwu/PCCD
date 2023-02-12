e1726599b9f9e8a75dcc6cb56c5b93ba17b360fa
lilruwu <101869123+lilruwu@users.noreply.github.com>
2023-02-12 18:46:37 +0000

    Update p1.c

--- p1.c
+++ p1.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 int main() {
 int i;
-int hijos_fin = 0;
+int hijos_finalizados = 0;
 
 for (i=0;i<3;i++){
     pid_t pid[3];
@@ -16,16 +17,18 @@ for (i=0;i<3;i++){
     } else if (pid[i]==0) {
         printf("Soy el hijo, voy a mimi\n");
         sleep(5);
-        printf("Ya he despertado!\n");
+        //printf("Ya he despertado!\n");
+        pid_t hijo_finalizado = wait(NULL);
+    	hijos_finalizados++;
+    	printf("El hijo con ID %d finalizó su ejecución\n", hijo_finalizado);
     }
 }
 
   while (hijos_finalizados < 3) {
-    pid_t hijo_finalizado = wait(NULL);
-    hijos_finalizados++;
-    printf("El hijo con ID %d finalizó su ejecución\n", hijo_finalizado);
+    
     pause();
   }
     printf("Todos los hijos han finalizado su ejecución\n");
+    exit(0);
     return 0;
 }
