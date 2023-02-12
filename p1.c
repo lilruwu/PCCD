d9e5223eb9f0f8fabaaa93dbee00c7c2ab80842f
lilruwu <101869123+lilruwu@users.noreply.github.com>
2023-02-12 19:49:27 +0100

    Update p1.c

--- p1.c
+++ p1.c
@@ -2,33 +2,34 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main() {
-int i;
-int hijos_finalizados = 0;
+  int i;
+  int hijos_finalizados = 0;
+  pid_t pid[3];
 
-for (i=0;i<3;i++){
-    pid_t pid[3];
+  for (i = 0; i < 3; i++) {
     pid[i] = fork();
 
-    if (pid[i]==-1) {
-        perror("Error al crear el proceso hijo");
-        return 1;
-    } else if (pid[i]==0) {
-        printf("Soy el hijo, voy a mimi\n");
-        sleep(5);
-        //printf("Ya he despertado!\n");
-        pid_t hijo_finalizado = wait(NULL);
-    	hijos_finalizados++;
-    	printf("El hijo con ID %d finalizó su ejecución\n", hijo_finalizado);
+    if (pid[i] == -1) {
+      perror("Error al crear el proceso hijo");
+      return 1;
+    } else if (pid[i] == 0) {
+      printf("Soy el hijo %d, durmiendo por 5 segundos...\n", i + 1);
+      sleep(5);
+      return 0;
     }
-}
+  }
 
+  // Proceso padre
   while (hijos_finalizados < 3) {
-    
+    pid_t hijo_finalizado = wait(NULL);
+    hijos_finalizados++;
+    printf("El hijo con ID %d finalizó su ejecución\n", hijo_finalizado);
     pause();
   }
-    printf("Todos los hijos han finalizado su ejecución\n");
-    exit(0);
-    return 0;
+
+  printf("Todos los hijos han finalizado su ejecución\n");
+  return 0;
 }
