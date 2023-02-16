3553289739cf5e5ecda7af3de029d426877efa34
lilruwu <101869123+lilruwu@users.noreply.github.com>
2023-02-16 15:59:35 +0000

    Update p6x.c

--- Practica 2/p6x.c
+++ Practica 2/p6x.c
@@ -5,8 +5,8 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
-    printf("Soy el hijo %d, imprimiendo la lista de parámetros...\n", atoi(argv[0])+1);
-    execl("/bin/sh", "sh", "-c", "echo $*", argv[1], argv[2], argv[3], (char *)0);
+    //printf("Soy el hijo %d!\n", atoi(argv[0])+1);
+    execl("/bin/echo", "echo", "-n", argv[1], argv[2], argv[3], NULL);
     perror("Error al invocar a execl");
     return 1;
 }
