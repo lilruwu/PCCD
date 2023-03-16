eeddcd58d90c0886f0f75d9ea1b12fc1b0badb30
lilruwu <101869123+lilruwu@users.noreply.github.com>
2023-03-16 19:02:02 +0100

    Update p6m.c

--- Practica4SinEl6/P6/p6m.c
+++ Practica4SinEl6/P6/p6m.c
@@ -1,168 +1,181 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <signal.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <pthread.h>
+#include <unistd.h>
 #include <semaphore.h>
 
-//Semaforos ---------
+sem_t varE,EscStart[20],EscStop[20],LecStart[20],LecStop[20],semEsc,semLect,variable;;
+int N1,N2,N3;
+int lectores = 0;
+int quiere=0,escribiendo=0;
+void funcion_lector(int *num){
+  int id = *num;
+  int impr;
 
-sem_t sem_lleno;
-sem_t sem_escribir;
-sem_t sem_cola;
-sem_t sem_paso_e[20];
-sem_t sem_paso_l[20];
-sem_t sem_salir_e[20];
-sem_t sem_salir_l[20];
-
-//-------------------
-
-//se necesitan 2 variables una de cuantos lectores hay y otra de si se está escribiendo o no
-// el unico lector que pelea es el primero el ultimo es el que libera
-// las variables hay que protejerlas con lectores
-int ocupaciones;
-
-void * lectores(int * numero){
-
-  int num=(int)numero;
   while(1){
-    sem_wait(&sem_escribir);
-    while(){
-      printf("[lector %d] -> Esperando a intenta leer...\n",num);
-      sem_wait(&sem_paso_l[num]);
-      printf("[lector %d] -> Intentando leer...\n",num);
-      sem_wait(&sem_lleno);
-      ocupaciones++;
-      printf("[lector %d] -> Leyendo...\n",num);
-      sem_wait(&sem_salir_l[num]);
-      printf("[lector %d] -> Fin lectura\n",num);
-      ocupaciones--;
-      sem_post(&sem_lleno);
+
+    if(impr==0){
+      printf("\nEsperando a intentar leer... thread %d ",id);
+      impr=1;
     }
-    sem_post(&sem_escribir);
-  }
-  pthread_exit(NULL);
-  return 0;
-}
-
-void * escritores(int * numero){
-
-  int num=(int)numero;
-  while(1){
-    printf("[escritor %d] -> Esperando a intenta escribir...\n",num);
-    sem_wait(&sem_paso_e[num]);
-    //printf("OCUPACIONES: %d\n",ocupaciones);
-    while(ocupaciones!=0);
-    printf("[escritor %d] -> Intentando escribir...\n",num);
-    sem_wait(&sem_escribir);
-    printf("[escritor %d] -> Escribiendo...\n",num);
-    sem_wait(&sem_salir_e[num]);
-    printf("[escritor %d] -> Fin escritura\n",num);
-    sem_post(&sem_escribir);
-  }
-  pthread_exit(NULL);
-  return 0;
-}
-
-
-int main (int argc,char *argv[]){
-
-  int i;
-  int N1;//numero total de lectores
-  int N2;//numero maximo de lectores simultaneos
-  int N3;//numero maximo de escritores
-  ocupaciones=0;
-
-  if(argc!=4){
-    printf("Hacen falta 3 parametros\n");
-    exit(0);
-  }
-  else
-    {
-      if (atoi(argv[1])<atoi(argv[2])){
-	printf("El numero de lectores debe ser mayor que el numero maximo de lectores simultaneos\n");
-	exit(0);
+    sem_wait(&LecStart[id]);
+    printf("\nIntentando leer... thread %d ",id);
+    sem_wait(&variable);
+    lectores++;
+    if(lectores == 1){
+      sem_wait(&semEsc);
+      sem_wait(&varE);
+      if(quiere!=0){
+	sem_post(&semEsc);
+	while(quiere!=0){
+	  sem_post(&varE);
+	  sem_wait(&varE);
+	}
+	sem_wait(&semEsc);
       }
+      sem_post(&varE);
+      sem_post(&variable);
+
+    }else{
+      sem_post(&variable);
     }
+    sem_wait(&semLect);
+    printf("\nLeyendo ...thread %d",id);
+    sem_wait(&LecStop[id]);
+    printf("\nFin lectura thread %d.",id);
+    impr=0;
+    sem_post(&semLect);
+    sem_wait(&variable);
+    lectores --;
+    if(lectores==0){
+      sem_post(&semEsc);
+      sem_post(&variable);
+    }else{
+      sem_post(&variable);
+    }
+  }
+}
+   
+void funcion_escritor(int *num){
+  int id = *num;
+  int impr=0;
+
+  while(1){
+    if(impr==0){
+      printf("\nEsperando a intentar escribir... thread %d ",id);
+      impr=1;
+    }
+    sem_wait(&EscStart[id]);
+    sem_wait(&varE);
+    quiere++;
+    sem_post(&varE);
+    printf("\nIntentando escribir... thread %d",id);
+    sem_wait(&semEsc);
+    sem_wait(&varE);
+    quiere--;
+    sem_post(&varE);
+    printf("\nEscribiendo ...thread %d",id);
+    sem_wait(&EscStop[id]);
+    printf("\nFin escritura thread %d.",id);
+    impr=0;
+    sem_post(&semEsc);
+  }
+}
+
+
+int main(int argc,char *argv[]){
+
+  if(argc != 4){
+    printf("\nDebe introducir los valores N1, N2 y N3.\n");
+    return 0;
+  }
 
   N1=atoi(argv[1]);
   N2=atoi(argv[2]);
   N3=atoi(argv[3]);
 
-  pthread_t mythread[argc];
-  sem_init(&sem_lleno,0,N2);
-  sem_init(&sem_escribir,0,1);
+  char cadena[10];
+  int opcion,i;
+  pthread_t idlect[N1],idEsc[N3];
+  int idl[N1],ide[N3];
+  int lector,escritor;
 
-  for(i=0;i<N1;i++){ //Lectores
-   int valor = i;
-   sem_init(&sem_paso_l[i],0,0);
-   sem_init(&sem_salir_l[i],0,0);
-   pthread_create(&(mythread[i]),NULL,(void *)lectores,(void *)(valor+1));
+  //Creación de THREADS                                                                                                                                                                                                                                                    
+
+  for(i=0;i<N1;i++){
+
+    pthread_create(&idlect[i],NULL,(void*)funcion_lector,(void*)&idl[i]);
+    idl[i]=i;
+    sem_init(&LecStart[i],0,1);
+    sem_init(&LecStop[i],0,1);
+    sem_wait(&LecStop[i]);
+    sem_wait(&LecStart[i]);
+
   }
-  for(i=0;i<N3;i++){ //Escritores
-    int valor = i;
-    sem_init(&sem_paso_e[i],0,0);
-    sem_init(&sem_salir_e[i],0,0);
-    pthread_create(&(mythread[i]),NULL,(void *)escritores,(void *)(valor+1));
+
+  for(i=0;i<N3;i++){
+    pthread_create(&idEsc[i],NULL,(void*)funcion_escritor,(void*)&ide[i]);
+    ide[i]=i;
+    sem_init(&EscStart[i],0,1);
+    sem_init(&EscStop[i],0,1);
+    sem_wait(&EscStop[i]);
+    sem_wait(&EscStart[i]);
+
   }
+  //Iniciación de semaforos
 
-
+  sem_init(&semEsc,0,1);
+  sem_init(&semLect,0,N2);
+  sem_init(&variable,0,1);
+  sem_init(&varE,0,1);
   while(1){
-
-    printf("1 -> Intentar leer\n");
-    printf("2 -> Finalizar leer\n");
-    printf("3 -> Intentar escribir\n");
-    printf("4 -> Finalizar escribir\n");
-    printf("5 -> Salir\n");
-    printf("");
-
-    int sel=0;
-    int sel2=0;
-
-    scanf("%d",&sel);
-
-    switch(sel)
-      {
-      case 1:
-	printf("Introduce el numero del lector (del 1 al %d)\n",N1);
-	printf("");
-	scanf("%d",&sel2);
-	sem_post(&sem_paso_l[sel2]);
-	break;
-
-      case 2:
-	printf("Introduce el numero del lector (del 1 al %d)\n",N1);
-	printf("");
-	scanf("%d",&sel2);
-	sem_post(&sem_salir_l[sel2]);
-	break;
-
-      case 3:
-	printf("Introduce el numero del escritor (del 1 al %d)\n",N3);
-	printf("");
-	scanf("%d",&sel2);
-	sem_post(&sem_paso_e[sel2]);
-	break;
-
-      case 4:
-	printf("Introduce el numero del escritor (del 1 al %d)\n",N3);
-	printf("");
-	scanf("%d",&sel2);
-	sem_post(&sem_salir_e[sel2]);
-	break;
-      case 5:
-	printf("----FIN----\n");
-	exit(0);
-	break;
-
-      default:printf("Valor incorrecto\n");
-	exit(0);
-      }
+    printf("\n1.Intentar leer");
+    printf("\n2.Finalizar leer");
+    printf("\n3.Intentar escribir");
+    printf("\n4.Finalizar escribir.");
+    printf("\n5.Salir.\n");
+    fgets(cadena,10,stdin);
+    opcion=atoi(cadena);
+    switch(opcion){
+    case 1:
+      printf("\n>introduzca el número del lector de 1 a %d",N1);
+      fgets(cadena,10,stdin);
+      lector=atoi(cadena);
+      sem_post(&LecStart[lector-1]);
+      break;
+    case 2:
+      printf("\n>introduzca el número del lector de 1 a %d",N1);
+      fgets(cadena,10,stdin);
+      lector=atoi(cadena);
+      sem_post(&LecStop[lector-1]);
+      break;
+    case 3:
+      printf("\n>introduzca el número del escritor de 1 a %d",N3);
+      fgets(cadena,10,stdin);
+      escritor=atoi(cadena);
+      sem_post(&EscStart[escritor-1]);
+      break;
+    case 4:
+      printf("\n>introduzca el número del escritor de 1 a %d",N3);
+      fgets(cadena,10,stdin);
+      escritor=atoi(cadena);
+      sem_post(&EscStop[escritor-1]);
+      break;
+    case 5:
+      return 0;
+    default:
+      break;
+    }
 
   }
-  
   return 0;
- 
-}
+}  
+
+
+
+
+
+
+
+
+
