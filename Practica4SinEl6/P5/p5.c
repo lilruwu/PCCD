9e58854c0d0d5243b924e0b0023c896b0e5ae7fe
lilruwu <101869123+lilruwu@users.noreply.github.com>
2023-03-16 20:26:47 +0100

    Update p5.c

--- Practica4SinEl6/P5/p5.c
+++ Practica4SinEl6/P5/p5.c
@@ -7,51 +7,51 @@
 #define N 100
 
 // Declaración de variables globales
-sem_t sem_leer, sem_fin_lectura, sem_dentro;
-sem_t sem_aux_entrada_leer[20];
+sem_t sem_escribir, sem_fin_lectura, sem_dentro;
+sem_t sem_aux_entrada_escribir[20];
 sem_t sem_aux_entrada_fin[20];
-int n1, n3;
+int n1, n2;
 
-// Función que ejecutará cada hilo lector
-void* lector(void* arg) {
+// Función que ejecutará cada hilo escritor
+void* escritor(void* arg) {
     int id = *(int*) arg;
     while (1) {
-        printf("[Escritor %d] -> Esperando a intentar escribir...\n", id);
-        sem_wait(&sem_aux_entrada_leer[id]); //este semaforo es para esperar a que me pidan leer
-        printf("[Escritor %d] -> Intentando escribir...\n", id);
-        sem_wait(&sem_dentro); //este es cuando el tio esta leyendo
+        printf("[escritor %d] -> Esperando a intentar escribir...\n", id);
+        sem_wait(&sem_aux_entrada_escribir[id]); //este semaforo es para esperar a que me pidan escribir
+        printf("[escritor %d] -> Intentando escribir...\n", id);
+        sem_wait(&sem_dentro); //este es cuando el tio esta Escribiendo
         
-        printf("[Escritor %d] -> Escribiendo...\n", id);
-        sem_wait(&sem_aux_entrada_fin[id]); //este es cuando ha acabado de leer
-        printf("[Escritor %d] -> Fin escritura\n", id);
+        printf("[escritor %d] -> Escribiendo...\n", id);
+        sem_wait(&sem_aux_entrada_fin[id]); //este es cuando ha acabado de escribir
+        printf("[escritor %d] -> Fin lectura\n", id);
         sem_post(&sem_dentro); //esto es pq el pive acabo y ya puede entrar el siguiente
     }
     return NULL;
 }
 
 int main(int argc, char* argv[]) {
-    int i, opcion, lector_id;
-    pthread_t lectores[N];
+    int i, opcion, escritor_id;
+    pthread_t escritores[N];
 
     // Obtener argumentos
     if (argc != 2) {
         printf("Error: Debe ingresar dos argumentos enteros.\n");
         return -1;
     }
-    
-    n3 = atoi(argv[1]);
+    n1 = atoi(argv[1]);
+    n2 = 1;
 
     // Inicialización de semáforos
 
-    sem_init(&sem_dentro,0,n3);
+    sem_init(&sem_dentro,0,n2);
 
-    // Creación de hilos lectores
-    for (i = 0; i < n3; i++) {
+    // Creación de hilos escritores
+    for (i = 0; i < n1; i++) {
         int* id = malloc(sizeof(int));
         *id = i + 1;
-        pthread_create(&lectores[i], NULL, lector, id);
-        sem_init(&sem_leer, 0, 0);
-        sem_aux_entrada_leer[i]=sem_leer;
+        pthread_create(&escritores[i], NULL, escritor, id);
+        sem_init(&sem_escribir, 0, 0);
+        sem_aux_entrada_escribir[i]=sem_escribir;
         sem_init(&sem_fin_lectura, 0, 0);
         sem_aux_entrada_fin[i]=sem_fin_lectura;
     }
@@ -65,15 +65,15 @@ int main(int argc, char* argv[]) {
         printf("3. Salir\n");
         scanf("%d", &opcion);
         if (opcion == 1) {
-            printf("Ingrese el número del escritor (de 1 a %d):\n", n3);
-            scanf("%d", &lector_id);
-            printf("[Main] -> Escritor %d tiene acceso a escribir.\n", lector_id);
-            sem_post(&sem_aux_entrada_leer[lector_id]);
+            printf("Ingrese el número del escritor (de 1 a %d):\n", n1);
+            scanf("%d", &escritor_id);
+            printf("[Main] -> escritor %d tiene acceso a la lectura.\n", escritor_id);
+            sem_post(&sem_aux_entrada_escribir[escritor_id]);
         } else if (opcion == 2) {
-            printf("Ingrese el número del escritor (de 1 a %d):\n", n3);
-            scanf("%d", &lector_id);
-            printf("[Main] -> Escritor %d finalizó su escritura.\n", lector_id);
-            sem_post(&sem_aux_entrada_fin[lector_id]);
+            printf("Ingrese el número del escritor (de 1 a %d):\n", n1);
+            scanf("%d", &escritor_id);
+            printf("[Main] -> escritor %d finalizó su lectura.\n", escritor_id);
+            sem_post(&sem_aux_entrada_fin[escritor_id]);
         } else if (opcion == 3) {
             break;
         } else {
@@ -82,8 +82,8 @@ int main(int argc, char* argv[]) {
     }
 
     // Finalización del programa
-    for (i = 0; i < n3; i++) {
-        pthread_cancel(lectores[i]);
+    for (i = 0; i < n1; i++) {
+        pthread_cancel(escritores[i]);
     }
 
     return 0;
