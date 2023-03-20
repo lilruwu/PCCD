a629678a01382a05455a3045db75151f7902ea8b
lilruwu <101869123+lilruwu@users.noreply.github.com>
2023-03-20 17:12:33 +0100

    Add files via upload

---
+++ Practica6/p3.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+struct mensaje {
+    long tipo;
+    char texto[50];
+};
+
+int main(int argc, char const *argv[]) {
+    int cola, estado;
+    struct mensaje mensaje;
+
+    //Obtener identificador de la cola de mensajes creada por el proceso p1
+    cola = msgget(CLAVE, 0777);
+    if (cola == -1) {
+        perror("msgget");
+        exit(EXIT_FAILURE);
+    }
+
+    //Llenar el mensaje a enviar
+    mensaje.tipo = 1; //tipo de mensaje
+    sprintf(mensaje.texto, "Mensaje de prueba"); //contenido del mensaje
+
+    //Enviar el mensaje a la cola
+    estado = msgsnd(cola, &mensaje, sizeof(struct mensaje) - sizeof(long), 0);
+    if (estado == -1) {
+        perror("msgsnd");
+        exit(EXIT_FAILURE);
+    }
+    printf("Mensaje enviado: %s\n", mensaje.texto);
+
+    //Comprobar que el mensaje se ha almacenado en la cola correctamente
+    system("ipcs -q");
+
+    return 0;
+}
